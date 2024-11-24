dd6fd9aad0aa228d7b7d00673e3e75c376e5f239
rd-plamen-petkov <plamen.petkov@rewe-digital.com>
2024-11-24 09:52:07 +0200

    Add dynamodb table and s3 bucket

--- lib/exam-stack.ts
+++ lib/exam-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
+import {AttributeType, BillingMode, StreamViewType, Table} from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Bucket} from "aws-cdk-lib/aws-s3";
 
 export class ExamStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const metadataTable = new Table(this, 'metadataTable', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      sortKey: {
+        name: 'FileExtension',
+        type: AttributeType.STRING
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      stream: StreamViewType.NEW_AND_OLD_IMAGES,
+      timeToLiveAttribute: 'ttl'
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ExamQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fileStorage = new Bucket(this, 'fileStorage', {})
   }
 }
