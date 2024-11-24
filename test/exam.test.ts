1a4aae7c0a9f7cdf6ef744bd801511fcba9fa3a2
rd-plamen-petkov <plamen.petkov@rewe-digital.com>
2024-11-24 12:59:44 +0200

    add test cases

--- test/exam.test.ts
+++ test/exam.test.ts
@@ -1,6 +1,7 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Exam from '../lib/exam-stack';
+import * as cdk from 'aws-cdk-lib';
+import * as Exam from '../lib/exam-stack';
+import { Template } from 'aws-cdk-lib/assertions';
+import 'jest-cdk-snapshot';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/exam-stack.ts
@@ -14,4 +15,13 @@ test('SQS Queue Created', () => {
 //   template.hasResourceProperties('AWS::SQS::Queue', {
 //     VisibilityTimeout: 300
 //   });
+});
+
+
+test('Test snapshot', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new Exam.ExamStack(app, 'MyTestStack');
+    // THEN
+    expect(stack).toMatchCdkSnapshot();
 });
